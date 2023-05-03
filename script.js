5fa8d517792dcc442fdbd0f327fa088657a44dd3
Frank-Colorado <chawnkiee@gmail.com>
2023-05-02 21:50:05 -0400

    created 'readmeGenerator' function in script.js

--- script.js
+++ script.js
@@ -1,2 +1,35 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+const readmeGenerator = ({}) => {
+  `#
+
+    ## Description 
+    
+    ## Table of Contents
+
+    ## Visuals 
+    
+    
+    ## Website URL 
+    
+    
+    ## Installation 
+    
+    
+    ## Usage
+   
+    
+    ## Credits 
+
+    ## License 
+    Please refer to the LICENSE in the repo.
+    
+    ## Contributing
+
+    ## Tests 
+
+    ## Questions 
+    
+    `;
+};
